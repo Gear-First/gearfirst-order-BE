41b791978166098c7ba9682087340b6aabb91887
ksoheee <thgml0716030@naver.com>
2025-11-06 16:51:56 +0900

    feat: 컨트롤러 파라미터에서 @CurrentUser를 통해 로그인 사용자 정보를 자동 주입하도록 ArgumentResolver 추가

---
+++ src/main/java/com/gearfirst/backend/common/resolver/CurrentUserArgumentResolver.java
@@ -0,0 +1,29 @@
+package com.gearfirst.backend.common.resolver;
+
+import com.gearfirst.backend.common.annotation.CurrentUser;
+import com.gearfirst.backend.common.context.UserContext;
+import com.gearfirst.backend.common.context.UserContextHolder;
+import com.gearfirst.backend.common.exception.UnAuthorizedException;
+import com.gearfirst.backend.common.response.ErrorStatus;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(CurrentUser.class)
+                && parameter.getParameterType().equals(UserContext.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        UserContext user = UserContextHolder.get();
+        if(user == null) {
+            throw new UnAuthorizedException(ErrorStatus.USER_UNAUTHORIZED.getMessage());
+        }
+        return user;
+    }
+}
