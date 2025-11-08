12ea81b76550882d0a57b0139996379528db2383
ksoheee <thgml0716030@naver.com>
2025-11-08 23:35:30 +0900

    발주 상태 이름 수정

--- src/main/java/com/gearfirst/backend/common/enums/OrderStatus.java
+++ src/main/java/com/gearfirst/backend/common/enums/OrderStatus.java
@@ -4,7 +4,7 @@ public enum OrderStatus {
     PENDING("승인 대기"),
     APPROVED("승인 완료"),
     REJECTED("반려"),
-    SHIPPED("출고 중"),
+    SHIPPED("출고"),
     COMPLETED("납품 완료"),
     CANCELLED("취소");
 
